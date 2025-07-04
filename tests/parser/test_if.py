617cfd2ba859fff4dfe28e982acbf9d6ecb97fd4
jbjd <jimdemetris@yahoo.com>
2025-07-04 12:57:09 -0500

    Add additional If node test

--- tests/parser/test_if.py
+++ tests/parser/test_if.py
@@ -1,7 +1,7 @@
 from tests.utils import BeforeAndAfter, run_minifier_and_assert_correct
 
 
-def test_if_with_only_passes():
+def test_if_pass():
     before_and_after = BeforeAndAfter(
         """
 if a == b:pass;pass
@@ -20,6 +20,21 @@ elif b == c:print()
 else:print()""",
         """
 if b==c:print()
+else:print()
+""".strip(),
+    )
+
+    run_minifier_and_assert_correct(before_and_after)
+
+
+def test_elif_pass():
+    before_and_after = BeforeAndAfter(
+        """
+if a == b:print()
+elif b == c:pass
+else:print()""",
+        """
+if a==b:print()
 else:print()
 """.strip(),
     )

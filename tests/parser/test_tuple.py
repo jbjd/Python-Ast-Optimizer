4508fe9757b96607d33e53046b13b285e9af0a50
James Demetris <63110925+jbjd@users.noreply.github.com>
2025-07-04 12:55:30 -0500

    Remove If nodes with only passes (#21)

--- tests/parser/test_tuple.py
+++ tests/parser/test_tuple.py
@@ -5,9 +5,9 @@ def test_tuple_whitespace():
     before_and_after = BeforeAndAfter(
         """
 if a in (1,2):
-    pass
+    print()
 """,
-        "if a in(1,2):pass",
+        "if a in(1,2):print()",
     )
 
     run_minifier_and_assert_correct(before_and_after)

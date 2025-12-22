440c54f2697747972673f24e78c85ae440956711
James D <63110925+jbjd@users.noreply.github.com>
2025-12-21 20:57:29 -0600

    Improvement: Avoid AstNodeSkipper's unnecessary list recreation when certain config values are empty (#42)

--- personal_python_ast_optimizer/futures.py
+++ personal_python_ast_optimizer/futures.py
@@ -1,22 +1,31 @@
-futures_to_mandatory_version: dict[str, tuple[int, int]] = {
-    "nested_scopes": (2, 2),
-    "generators": (2, 3),
-    "with_statement": (2, 6),
-    "division": (3, 0),
-    "absolute_import": (3, 0),
-    "print_function": (3, 0),
-    "unicode_literals": (3, 0),
-    "generator_stop": (3, 7),
-}
+class Futures:
+
+    __slots__ = ("name", "mandatory_version")
+
+    def __init__(self, name: str, mandatory_version: tuple[int, int]) -> None:
+        self.name: str = name
+        self.mandatory_version: tuple[int, int] = mandatory_version
+
+
+futures_to_mandatory_version: list[Futures] = [
+    Futures("nested_scopes", (2, 2)),
+    Futures("generators", (2, 3)),
+    Futures("with_statement", (2, 6)),
+    Futures("division", (3, 0)),
+    Futures("absolute_import", (3, 0)),
+    Futures("print_function", (3, 0)),
+    Futures("unicode_literals", (3, 0)),
+    Futures("generator_stop", (3, 7)),
+]
 
 
 def get_unneeded_futures(python_version: tuple[int, int]) -> list[str]:
     """Returns list of __future__ imports that are unneeded in provided
     python version"""
     unneeded_futures: list[str] = [
-        future
-        for future, mandatory_version in futures_to_mandatory_version.items()
-        if python_version >= mandatory_version
+        future.name
+        for future in futures_to_mandatory_version
+        if python_version >= future.mandatory_version
     ]
 
     return unneeded_futures

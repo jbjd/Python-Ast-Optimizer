04a86c9a4d57e552319d79cc132f499098c93ebb
jbjd <jimdemetris@yahoo.com>
2025-12-22 19:54:51 -0600

    Use most accurate type hint for vars_to_fold

--- personal_python_ast_optimizer/parser/config.py
+++ personal_python_ast_optimizer/parser/config.py
@@ -1,7 +1,10 @@
 from abc import abstractmethod
-from ast import _ConstantValue
 from enum import Enum, EnumType
+from types import EllipsisType
 from typing import Iterable, Iterator
+
+# I tried to import this from ast, it worked on 3.12 but not 3.11?
+type _ConstantValue = str | bytes | bool | int | float | complex | None | EllipsisType
 
 
 class TokensToSkip(dict[str, int]):

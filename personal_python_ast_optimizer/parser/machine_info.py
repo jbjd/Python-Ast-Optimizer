d1fa0a696a2a20dcead7f31703d61fab64bfe08d
jbjd <jimdemetris@yahoo.com>
2026-01-12 21:29:15 -0600

    Improvement: Set sys.platform to machine_info

--- personal_python_ast_optimizer/parser/machine_info.py
+++ personal_python_ast_optimizer/parser/machine_info.py
@@ -4,6 +4,7 @@ import sys
 machine_dependent_attributes: dict[str, str] = {
     "os.name": os.name,
     "sys.byteorder": sys.byteorder,
+    "sys.platform": sys.platform,
 }
 
 machine_dependent_functions: dict[str, int | None] = {"os.cpu_count": os.cpu_count()}

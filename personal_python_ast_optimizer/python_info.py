b299bba1b74ae3ceabe4a471e4168d97d405b4eb
jbjd <jimdemetris@yahoo.com>
2026-01-17 20:00:15 -0600

    add more to defaults

--- personal_python_ast_optimizer/python_info.py
+++ personal_python_ast_optimizer/python_info.py
@@ -1,10 +1,16 @@
 """Various tokens in Python that the ast module writes"""
 
-# Functions that have no sideeffects and thus are safe to remove
+# Functions that have no side effects and thus are safe to remove
 # if a test expression is found to be useless. For example:
 # if "str(a) == 'a':pass" will be turned into just "str(a) == 'a'"
 # but if its known str has no side effects then it can be fully removed
-default_functions_safe_to_exclude_in_test_expr: set[str] = {"int", "str", "isinstance"}
+default_functions_safe_to_exclude_in_test_expr: set[str] = {
+    "int",
+    "str",
+    "isinstance",
+    "getattr",
+    "hasattr",
+}
 
 comparison_and_conjunctions: list[str] = [
     " if ",

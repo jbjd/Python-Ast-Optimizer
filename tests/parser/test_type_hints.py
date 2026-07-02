3d7c77cbdd4082cfd428a6298ef83a8fa872fbe8
jbjd <jimdemetris@yahoo.com>
2026-07-01 23:32:25 -0500

    Dev: Fix test

--- tests/parser/test_type_hints.py
+++ tests/parser/test_type_hints.py
@@ -1,3 +1,5 @@
+import pytest
+
 from personal_python_ast_optimizer.parser.config import (
     TokenTypesConfig,
     TypeHintsToSkip,
@@ -51,4 +53,51 @@ def test_remove_all_type_hints():
     run_minifier_and_assert_correct(
         before_and_after,
         token_types_config=TokenTypesConfig(skip_type_hints=TypeHintsToSkip.ALL),
+    )
+
+
+def test_generics_config():
+    with pytest.raises(
+        ValueError, match="Can't skip Generics if not skipping type hints"
+    ):
+        TokenTypesConfig(skip_type_hints=TypeHintsToSkip.NONE, skip_generics=True)
+
+
+def test_generics_keep():
+    before_and_after = BeforeAndAfter(
+        """
+def a[FOO, BAR](a: FOO, b: BAR) -> None:
+    print(a, b)
+class A[FOO]:
+    def __init__(self,a:FOO):
+        self.a:FOO=a
+""",
+        """def a[FOO,BAR](a,b):print(a,b)
+class A[FOO]:\n\tdef __init__(self,a):self.a=a""",
+    )
+
+    run_minifier_and_assert_correct(before_and_after)
+
+
+@pytest.mark.parametrize(
+    "skip_type_hints", [TypeHintsToSkip.ALL, TypeHintsToSkip.ALL_BUT_CLASS_VARS]
+)
+def test_generics_remove(skip_type_hints: TypeHintsToSkip):
+    before_and_after = BeforeAndAfter(
+        """
+def a[FOO, BAR](a: FOO, b: BAR) -> None:
+    print(a, b)
+class A[FOO]:
+    def __init__(self,a:FOO):
+        self.a:FOO=a
+""",
+        """def a(a,b):print(a,b)
+class A:\n\tdef __init__(self,a):self.a=a""",
+    )
+
+    run_minifier_and_assert_correct(
+        before_and_after,
+        token_types_config=TokenTypesConfig(
+            skip_type_hints=skip_type_hints, skip_generics=True
+        ),
     )

bf7652f260384fe483ec8d98167a6c11eae603ff
jbjd <jimdemetris@yahoo.com>
2026-08-27 22:51:52 -0500

    Minor optimizations

--- personal_python_ast_optimizer/minifier.py
+++ personal_python_ast_optimizer/minifier.py
@@ -276,10 +276,9 @@ class MinifyUnparser(ast._Unparser):  # type: ignore[misc, name-defined]
 
     def visit_AnnAssign(self, node: ast.AnnAssign) -> None:
         self.fill_splitter()
-        with self.delimit_if(
-            "(", ")", not node.simple and isinstance(node.target, ast.Name)
-        ):
-            self.traverse(node.target)
+        if node.simple == 0 and isinstance(node.target, ast.Name):
+            with self.delimit("(", ")"):
+                self.traverse(node.target)
 
         self._source.append(":")
         self.traverse(node.annotation)
---
+++ tests/optimizer/test_node_assign.py
@@ -0,0 +1,13 @@
+from personal_python_ast_optimizer.config import TokenTypesToSkipConfig, TypeHintsToSkip
+from tests.utils import optimize_and_assert_correctness
+
+
+def test_not_simple_ann_assign():
+    """Should wrap AnnAssign in paranthesis."""
+    optimize_and_assert_correctness(
+        "(a): int = 1",
+        "(a):int=1",
+        token_types_to_skip=TokenTypesToSkipConfig(
+            skip_type_hints=TypeHintsToSkip.NONE
+        ),
+    )

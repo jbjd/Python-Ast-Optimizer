c352630d0801efee98be72a4d4808a44ec8ae5ea
jbjd <jimdemetris@yahoo.com>
2026-08-27 22:58:45 -0500

    spell

--- tests/optimizer/test_node_assign.py
+++ tests/optimizer/test_node_assign.py
@@ -3,7 +3,7 @@ from tests.utils import optimize_and_assert_correctness
 
 
 def test_not_simple_ann_assign():
-    """Should wrap AnnAssign in paranthesis."""
+    """Should wrap AnnAssign in parenthesis."""
     optimize_and_assert_correctness(
         "(a): int = 1",
         "(a):int=1",
